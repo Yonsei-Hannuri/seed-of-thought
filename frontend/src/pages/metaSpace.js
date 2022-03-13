2ab9c2f1a7ad6d7bb210e652fbf37a2cebdffd36
habibi <jnh03336@gmail.com>
2022-03-13 15:51:47 +0900

    minor: asset image change and chore

--- frontend/src/pages/metaSpace.js
+++ frontend/src/pages/metaSpace.js
@@ -4,7 +4,7 @@ function MetaSpace(props) {
         <img
           name='freeNote' onClick={props.pageSelect}
           className="m-3 p-3 cursor2Pointer"
-          src="https://image.flaticon.com/icons/png/512/3324/3324709.png"
+          src="notebook.png"
           width="220"
           height="220"
           alt="Notebook  free icon"
