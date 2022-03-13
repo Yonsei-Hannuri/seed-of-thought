6f84a851b1b763c07aa6a640088b46ca2d49bca6
habibi <jnh03336@gmail.com>
2022-03-13 18:36:42 +0900

    Refactor: split wordchart from the sessionPage

---
+++ frontend/src/components/session/nameCard.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import httpsfy from '../../modules/httpsfy';
+import axios from 'axios';
+
+class NameCard extends Component {
+    static defaultProps = {
+      onClick: null,
+      detgoriUrl: null,
+    };
+  
+    state = {
+      detgoriInfo: { authorName: '', googleId: '' },
+      currentDetgoriId: null,
+    };
+  
+    componentDidMount() {
+      axios({
+        method: 'GET',
+        url: httpsfy(this.props.detgoriUrl, process.env.NODE_ENV),
+        withCredentials: true,
+      })
+        .then((res) => res.data)
+        .then((data) => {
+          this.setState({ detgoriInfo: data });
+        });
+    }
+  
+    render() {
+      const authorColor = this.state.detgoriInfo.authorColor
+      return (
+        <div>
+          <button
+            style={{
+              border: `2px solid ${authorColor}`,
+              boxShadow: `0px 0px 3px ${authorColor}`
+            }}
+            className="btn m-1 btn-light"
+            value={this.state.detgoriInfo.googleId}
+            onClick={this.props.onClick}
+          >
+            {this.state.detgoriInfo.authorName}
+          </button>
+        </div>
+      );
+    }
+  }
+
+  export default NameCard;
--- frontend/src/pages/session.js
+++ frontend/src/pages/session.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Bar } from 'react-chartjs-2';
-import SessionReadfile from '../components/App/session/sessionReadfile';
+import SessionReadfile from '../components/session/sessionReadfile';
 import errorReport from '../modules/errorReport';
 import address from '../config/address.json';
 import axios from 'axios';
 import { Page, Document } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
-import NameCard from '../components/App/session/nameCard';
+import NameCard from '../components/session/nameCard';
+import WordChart from '../components/session/wordChart/wordChart';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 class Session extends Component {
@@ -54,7 +54,7 @@ class Session extends Component {
       validateStatus: function (status) {
         // 상태 코드가 200대가 아닐 경우 //이부분 반드시 필요 없는듯
         if (!(status < 300 && status >= 200)) {
-          //window.location.href = address.front;
+          window.location.href = address.front;
           return false;
         }
         return true;
@@ -71,85 +71,8 @@ class Session extends Component {
           .then((res) => res.json())
           .then((data_) => {
             let data = data_.wordList;
-            if (data.length > 0) {
-              const words = JSON.parse(data[0]);
-              for (let i = 1; i < data.length; i++) {
-                let words_ = JSON.parse(data[i]);
-                for (let key in words_) {
-                  if (Object.keys(words).includes(key)) {
-                    words[key] += words_[key];
-                  } else {
-                    words[key] = words_[key];
-                  }
-                }
-              }
-              // make a rank
-              let wordArray = [];
-              for (let key in words) {
-                let count = words[key];
-                wordArray.push([key, count]);
-              }
-              wordArray.sort(function (a, b) {
-                return -(a[1] - b[1]);
-              });
-
-              // chart config
-              let chartData = {
-                labels: wordArray.slice(0, 5).map((item) => item[0]), //많이 언급된 단어 라벨
-                datasets: [
-                  {
-                    label: '', //이건 왜 있는 걸까?
-                    data: wordArray.slice(0, 5).map((item) => item[1]), //언급 횟수
-                    backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)',
-                    ],
-                    borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)',
-                    ],
-                    borderWidth: 1,
-                  },
-                ],
-              };
-              const options = {
-                indexAxis: 'y',
-                // Elements options apply to all of the options unless overridden in a dataset
-                // In this case, we are setting the border of each horizontal bar to be 2px wide
-                elements: {
-                  bar: {
-                    borderWidth: 2,
-                  },
-                },
-                responsive: true,
-                plugins: {
-                  legend: {
-                    position: 'right',
-                  },
-                  title: {
-                    display: true,
-                    text: '단어 언급 횟수',
-                  },
-                },
-              };
-              const chartDiv = (
-                <div className="mt-3 py-3" id="statics">
-                  <span className="fw-bolder fs-4 m-0">통계</span>
-                  <div className="maxWidth m-auto">
-                    <Bar data={chartData} options={options} />
-                  </div>
-                </div>
-              );
-              this.setState({ chartDiv: chartDiv, info: sessionData });
-            }
+            if (data.length === 0) return;
+            this.setState({ chartDiv: <WordChart data={data}/>, info: sessionData });
           })
           .catch((e) => errorReport(e, 'front-session'));
       })
