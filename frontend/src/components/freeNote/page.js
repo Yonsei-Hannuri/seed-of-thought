fed40dc2b6adc756cf2d52a889dbf3f2620968e3
habibi <jnh03336@gmail.com>
2022-06-11 14:44:19 +0900

    refactor

--- frontend/src/components/freeNote/page.js
+++ frontend/src/components/freeNote/page.js
@@ -15,18 +15,6 @@ export default class Page extends Component {
       writingPosition: -1,
     };
   
-    componentDidMount() {
-      let newInfoState = [false, false, false, false, false, false, false, false];
-      for (let i = 0; i < this.props.info.length; i++) {
-        let note = this.props.info[i];
-        newInfoState = newInfoState
-          .slice(0, note.position)
-          .concat([note])
-          .concat(newInfoState.slice(note.position + 1, 8));
-      }
-      this.setState({ info: newInfoState });
-    }
-  
     componentDidUpdate(prevProps, prevState) {
       //when clicked for update => set state which note is selected(only one at once)
       if (prevState.writingPosition !== this.state.writingPosition) {
@@ -58,12 +46,8 @@ export default class Page extends Component {
           false,
           false,
         ];
-        for (let i = 0; i < this.props.info.length; i++) {
-          let note = this.props.info[i];
-          newInfoState = newInfoState
-            .slice(0, note.position)
-            .concat([note])
-            .concat(newInfoState.slice(note.position + 1, 8));
+        for(let note of this.props.info){
+          newInfoState[note.position] = note;
         }
         this.setState({ info: newInfoState, writingPosition: -1 });
       }
@@ -74,97 +58,24 @@ export default class Page extends Component {
     };
   
     render() {
+      const notes = this.state.info.map((_ ,idx)=> {
+        return (
+            <div style={{width:'50%'}} className="p-1 h-rem15" key={idx}>
+              <Note
+                info={this.state.info[idx]}
+                onClick={this.handleClick}
+                onUpload={this.props.onUpload}
+                page={this.props.page}
+                position={idx}
+                writing={this.state.writing[idx]}
+              />
+            </div>
+        )
+      })
       return (
-        <div>
-          <div className="row">
-            <div className="p-2 col-sm-6 h-rem15">
-              <Note
-                info={this.state.info[0]}
-                onClick={this.handleClick}
-                onUpload={this.props.onUpload}
-                page={this.props.page}
-                position={0}
-                writing={this.state.writing[0]}
-              />
-            </div>
-            <div className="p-2 col-sm-6 h-rem15">
-              <Note
-                info={this.state.info[1]}
-                onClick={this.handleClick}
-                onUpload={this.props.onUpload}
-                page={this.props.page}
-                position={1}
-                writing={this.state.writing[1]}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="p-2 col-sm-6 h-rem15">
-              <Note
-                info={this.state.info[2]}
-                onClick={this.handleClick}
-                onUpload={this.props.onUpload}
-                page={this.props.page}
-                position={2}
-                writing={this.state.writing[2]}
-              />
-            </div>
-            <div className="p-2 col-sm-6 h-rem15">
-              <Note
-                info={this.state.info[3]}
-                onClick={this.handleClick}
-                onUpload={this.props.onUpload}
-                page={this.props.page}
-                position={3}
-                writing={this.state.writing[3]}
-              />
-            </div>
-          </div>
-          <div className="row =">
-            <div className="p-2 col-sm-6 h-rem15">
-              <Note
-                info={this.state.info[4]}
-                onClick={this.handleClick}
-                onUpload={this.props.onUpload}
-                page={this.props.page}
-                position={4}
-                writing={this.state.writing[4]}
-              />
-            </div>
-            <div className="p-2 col-sm-6 h-rem15">
-              <Note
-                info={this.state.info[5]}
-                onClick={this.handleClick}
-                onUpload={this.props.onUpload}
-                page={this.props.page}
-                position={5}
-                writing={this.state.writing[5]}
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="p-2 col-sm-6 h-rem15">
-              <Note
-                info={this.state.info[6]}
-                onClick={this.handleClick}
-                onUpload={this.props.onUpload}
-                page={this.props.page}
-                position={6}
-                writing={this.state.writing[6]}
-              />
-            </div>
-            <div className="p-2 col-sm-6 h-rem15">
-              <Note
-                info={this.state.info[7]}
-                onClick={this.handleClick}
-                onUpload={this.props.onUpload}
-                page={this.props.page}
-                position={7}
-                writing={this.state.writing[7]}
-              />
-            </div>
-          </div>
+        <div style={{display:'flex', width:'100%',flexWrap:'wrap'}}>
+          {notes}
         </div>
-      );
+      )
     }
   }
