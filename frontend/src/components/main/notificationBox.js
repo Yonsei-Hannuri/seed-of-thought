3449d22f2c1769b6068ede44fb6c7279b5b6e77c
habibi <jnh03336@gmail.com>
2022-06-13 18:18:21 +0900

    refactor: class to function component

--- frontend/src/components/main/notificationBox.js
+++ frontend/src/components/main/notificationBox.js
@@ -1,80 +1,60 @@
-import React, { Component } from 'react';
+import { useState } from "react";
 
-class NotificationBox extends Component {
-  static defaultProps = {
-    notifications: [],
-  };
-  render() {
-    const notification_list = this.props.notifications.map((info, idx) => (
-      <Notification index={idx + 1} info={info} key={info.id} />
-    ));
-    if (notification_list.length !== 0) {
-      return (
-        <div className="my-5">
-          <div className="row p-4 align-items-center rounded-3 border shadow-lg">
-            <div className="text-start">
-              <h4>공지사항</h4>
-            </div>
-            <table className="table">
-              <tbody>{notification_list}</tbody>
-            </table>
+export default function NotificationBox({notifications=[]}) {
+  const notification_list = notifications.map((info, idx) => (
+    <Notification index={idx + 1} info={info} key={info.id} />
+  ));
+  if (notification_list.length !== 0) {
+    return (
+      <div className="my-5">
+        <div className="row p-4 align-items-center rounded-3 border shadow-lg">
+          <div className="text-start">
+            <h4>공지사항</h4>
           </div>
+          <table className="table">
+            <tbody>{notification_list}</tbody>
+          </table>
         </div>
-      );
-    } else {
-      return '';
-    }
+      </div>
+    );
+  } else {
+    return '';
   }
 }
 
-class Notification extends Component {
-  state = {
-    clicked: false,
-  };
-
-  static defaultProps = {
-    index: 0,
-    info: {},
-  };
-
-  handleClick = () => {
-    this.setState({ clicked: this.state.clicked ? false : true });
-  };
-  render() {
-    if (this.state.clicked === true) {
+function Notification({index, info}){
+  const [clicked, setClicked] = useState(false);
+    if (clicked === true) {
       return (
         <tr>
-          <th className="col-3 text-center">{this.props.index}</th>
+          <th className="col-3 text-center">{index}</th>
           <td>
             <h3>
-              {this.props.info.title}
+              {info.title}
               <button
                 className="float-end link-button"
-                onClick={this.handleClick}
+                onClick={()=>setClicked(!clicked)}
               >
                 X
               </button>
             </h3>
-            <p>{this.props.info.date.slice(0, 10)}</p>
-            <p>{this.props.info.description}</p>
+            <p>{info.date.slice(0, 10)}</p>
+            <p>{info.description}</p>
           </td>
         </tr>
       );
     }
     return (
       <tr>
-        <th className="col-3 text-center">{this.props.index}</th>
+        <th className="col-3 text-center">{index}</th>
         <td>
-          <button
+          <div
             className="col-9 text-start link-button"
-            onClick={this.handleClick}
+            onClick={()=>setClicked(!clicked)}
           >
-            {this.props.info.title}
-          </button>
+            {info.title}
+          </div>
         </td>
       </tr>
     );
-  }
 }
-
-export default NotificationBox;
--- frontend/src/components/main/sessionBanner.js
+++ frontend/src/components/main/sessionBanner.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
-
-class SessionBanner extends Component {
-  static defaultProps = {
-    recentSession: {},
-  };
-
-  render() {
-    if (this.props.recentSession.week){
+export default function SessionBanner({recentSession={}}) {
+    if (recentSession.week){
         return (
         <div>
             <div className="px-4 py-5 my-5 text-center">
             <h3 className="display-5 fw-normal">
-                {this.props.recentSession.week} 주차
+                {recentSession.week} 주차
             </h3>
             <h1 className="display-5 fw-bold">
-                {this.props.recentSession.title}
+                {recentSession.title}
             </h1>
             <div className="col-lg-6 mx-auto">
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <a href={'/session/?sessionID=' + this.props.recentSession.id}>
+                <a href={'/session/?sessionID=' + recentSession.id}>
                     <button
                     type="button"
                     className="btn btn-light border btn-lg px-4 gap-3"
@@ -34,7 +27,4 @@ class SessionBanner extends Component {
     } else {
         return '';
     }
-  }
 }
-
-export default SessionBanner;
