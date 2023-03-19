8122f04d3b555005369310fd50c590b40290cf82
하지훈 <jnh03336@gmail.com>
2023-03-19 16:15:38 +0900

    [FE] update&refactor: remove signin feat, class to functional component

--- frontend/src/components/login/loginForm.js
+++ frontend/src/components/login/loginForm.js
@@ -1,24 +1,23 @@
-function LoginForm (props){ 
-    //loginUrl, sininSuccess, loginError
-    return (  
-        <main>
-            <h1>생각의 씨앗,</h1>
-            <h1>완전한 만남.</h1>
-            <div className="lead my-3">
-            <a href={props.loginUrl}>
-                <img width="200px" src="/google.png" alt="google login" />
-            </a>
-            <p className="my-2 py-2">
-                {props.signinSuccess
-                ? '등록되었습니다. 임원진 확인 후 로그인 할 수 있습니다.'
-                : ''}
-            </p>
-            <p className="my-2 py-2">
-                {props.loginError ? '로그인에 실패했습니다.' : ''}
-            </p>
-            </div>
-        </main>
-    )
+/**
+ *
+ * @param {{ googleUrl, isError }} props
+ * @returns
+ */
+function LoginForm(props) {
+  return (
+    <main>
+      <h1>생각의 씨앗,</h1>
+      <h1>완전한 만남.</h1>
+      <div className="lead my-3">
+        <a href={props.googleUrl}>
+          <img width="200px" src="/google.png" alt="google login" />
+        </a>
+        <p className="my-2 py-2">
+          {props.isError ? '로그인에 실패했습니다.' : ''}
+        </p>
+      </div>
+    </main>
+  );
 }
 
 export default LoginForm;
--- frontend/src/pages/loginPage.js
+++ frontend/src/pages/loginPage.js
@@ -1,91 +1,22 @@
-import React, { Component } from 'react';
 import LoginForm from '../components/login/loginForm';
-import SignInForm from '../components/login/signinForm';
+import getQueryParams from '../modules/getQueryParams';
 
-class Login extends Component {
-  state = {
-    signinError: false,
-    signinSuccess: false,
-    login: true,
-    loginError: false,
-    signin: false,
-    name: '',
-    generation: 20,
-  };
-
-  componentDidMount() {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
-    if (params.type === 'signinError') {
-      this.setState({ signinError: true, signin: true, login: false });
-    } else if (params.type === 'signinSuccess') {
-      this.setState({ signinSuccess: true, signin: false, login: true });
-    } else if (params.login === 'error') {
-      this.setState({ loginError: true, login: true, signin: false });
-    }
-  }
-
-  handleClick = (e) => {
-    this.setState({
-      login: false,
-      signin: false,
-      [e.target.name]: true,
-    });
-  };
-
-  render() {
-    return (
-      <div className="d-flex h-100 text-center text-white bg-theme">
-        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-          <header className="mb-auto">
-            <div>
-              <h3 className="float-md-start mb-0">한누리</h3>
-              <nav className="nav nav-masthead justify-content-center float-md-end">
-                <button
-                  className={
-                    this.state.login
-                      ? 'nav-link active link-button2'
-                      : 'nav-link link-button2'
-                  }
-                  name="login"
-                  onClick={this.handleClick}
-                  href="#"
-                >
-                  로그인
-                </button>
-                {/*<a className="nav-link" href="#">홍보</a>*/}
-                <button
-                  className={
-                    this.state.signin
-                      ? 'nav-link active link-button2'
-                      : 'nav-link link-button2'
-                  }
-                  name="signin"
-                  onClick={this.handleClick}
-                  href="#"
-                >
-                  가입요청
-                </button>
-              </nav>
-            </div>
-          </header>
-          {this.state.login ? (
-            <LoginForm
-              loginUrl={process.env.REACT_APP_GOOGLE_OAUTH_LOGIN_URL}
-              signinSuccess={this.state.signinSuccess}
-              loginError={this.state.loginError}
-            />
-          ) : (
-            <SignInForm
-              signinError={this.state.signinError}
-              signinUrl={process.env.REACT_APP_GOOGLE_OAUTH_SIGNIN_URL}
-            />
-          )}
-          <footer className="mt-auto text-white-50"></footer>
-        </div>
+function Login() {
+  const isError = getQueryParams()?.login === 'error';
+  return (
+    <div className="d-flex h-100 text-center text-white bg-theme">
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header className="mb-auto">
+          <h3 className="float-md-start mb-0">한누리</h3>
+        </header>
+        <LoginForm
+          googleUrl={process.env.REACT_APP_GOOGLE_OAUTH_LOGIN_URL}
+          isError={isError}
+        />
+        <footer className="mt-auto text-white-50"></footer>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Login;
