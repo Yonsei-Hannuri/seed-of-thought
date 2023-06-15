76e4ae874a50421e5033dc0c00615fc26d8cf2e9
하지훈 <jnh03336@gmail.com>
2023-06-15 11:23:09 +0900

    [FE] refactor: keep format

--- frontend/src/components/main/sessionBanner.js
+++ frontend/src/components/main/sessionBanner.js
@@ -1,23 +1,17 @@
-export default function SessionBanner({recentSession={}, children}) {
-    if (recentSession.week){
-        return (
-        <div>
-            <div className="px-4 py-5 my-5 text-center">
-            <h3 className="display-5 fw-normal">
-                {recentSession.week} 주차
-            </h3>
-            <h1 className="display-5 fw-bold">
-                {recentSession.title}
-            </h1>
-            <div className="col-lg-6 mx-auto">
-                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                {children}
-                </div>
-            </div>
-            </div>
+export default function SessionBanner({ recentSession = {}, children }) {
+  if (recentSession.week) {
+    return (
+      <div className="text-center p-5">
+        <h3 className="display-5 fw-normal">{recentSession.week} 주차</h3>
+        <h1 className="display-5 fw-bold">{recentSession.title}</h1>
+        <div className="col-lg-6 mx-auto">
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            {children}
+          </div>
         </div>
-        );
-    } else {
-        return '';
-    }
+      </div>
+    );
+  } else {
+    return '';
+  }
 }
