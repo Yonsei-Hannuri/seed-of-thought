a6c869830b034b487ebb536a5e7f3c00fdc6f0ff
하지훈 <jnh03336@gmail.com>
2024-02-11 13:38:48 +0900

    deploy: use docker image (not docker compose)

--- .deploy/script/application-restart.sh
+++ .deploy/script/application-restart.sh
@@ -1,4 +1,7 @@
-cd /home/hannuri/
+cd /deploy
 
-sudo docker-compose stop
-sudo docker-compose up -d
+sudo docker stop hannuri-api || true
+sudo docker rm hannuri-api || true
+sudo docker image rm hannuri-api || true
+sudo docker load -i hannuri-api.tar
+sudo docker run -d --platform linux/amd64 -p 2000:2102 -v /home/hannuri/db:/backend/db --name hannuri-api hannuri-api
