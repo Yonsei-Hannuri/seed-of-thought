2762ed9b5a244bd488cd8b74a44e240eedd2d4b6
하지훈 <jnh03336@gmail.com>
2024-02-12 08:54:22 +0900

    refacotor: change google oauth secret file path, make it closer to the component

--- backend/hannuri/component/GoogleOauth/GoogleOauth.py
+++ backend/hannuri/component/GoogleOauth/GoogleOauth.py
@@ -1,13 +1,16 @@
+from pathlib import Path
+import os
 import requests
 import google_auth_oauthlib.flow
 
 class GoogleOauth:
     def __init__(self):
+        self.secret_path = os.path.join(Path(__file__).parent.absolute(), 'secret', 'client_secret.json')
         pass
 
     def get_user_email(self, code, redirect_url):
         flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-            './secret/client_secret.json',
+            self.secret_path,
             scopes = 'openid https://www.googleapis.com/auth/userinfo.email'
         )
         flow.redirect_uri = redirect_url
