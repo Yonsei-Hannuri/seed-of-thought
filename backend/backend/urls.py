44f27a48bc645715a78bca8c79f09f876f1e2b09
하지훈 <jnh03336@gmail.com>
2024-02-14 22:54:10 +0900

    fix: static files through django server is only available on the DEBUG mode
    https://docs.djangoproject.com/en/1.10/ref/contrib/staticfiles/#static-file-development-view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,15 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('hannuri.urls')),
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.STATIC_URL,  document_root=settings.STATIC_ROOT)
 
 
 if settings.ENV('DEBUG'):
+    from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,  document_root=settings.STATIC_ROOT)
+
