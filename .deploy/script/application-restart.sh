81a6d9196a3ddb5131b2795ae56f644d4f52ac38
하지훈 <jnh03336@gmail.com>
2024-04-19 09:16:04 +0900

    deploy: remove memory restriction
    - not solution for memory competition

--- .deploy/script/application-restart.sh
+++ .deploy/script/application-restart.sh
@@ -4,4 +4,4 @@ sudo docker stop hannuri-api || true
 sudo docker rm hannuri-api || true
 sudo docker image rm hannuri-api || true
 sudo docker load -i hannuri-api.tar
-sudo docker run -d --platform linux/amd64 -p 2999:2102 -v /home/hannuri/log:/backend/log -v /home/hannuri/db:/backend/db --network hannuri-net -m 300m --name hannuri-api hannuri-api
+sudo docker run -d --platform linux/amd64 -p 2999:2102 -v /home/hannuri/log:/backend/log -v /home/hannuri/db:/backend/db --network hannuri-net --name hannuri-api hannuri-api
