86c0140e10fa85009eb1c64da8e5a9e3f77b6463
하지훈 <jnh03336@gmail.com>
2024-07-08 18:27:48 +0900

    feat: add create_dt, updated_dt to entities

--- backend/cowriter/models.py
+++ backend/cowriter/models.py
@@ -1,38 +1,38 @@
 from django.db import models
-
-class Subject(models.Model):
+from common.models.TimeStampMixin import TimeStampMixin
+class Subject(TimeStampMixin):
     source = models.CharField(max_length=200, verbose_name="출처")
     purpose = models.CharField(max_length=200, verbose_name="목적")
     content = models.TextField(blank=True)
 
-class Keyword(models.Model):
+class Keyword(TimeStampMixin):
     name = models.CharField(max_length=200, blank=True, verbose_name="이름")
     description = models.CharField(max_length=200, blank=True, verbose_name="이름")
 
-class SubjectKeyword(models.Model):
+class SubjectKeyword(TimeStampMixin):
     subject = models.ForeignKey(Subject, on_delete=models.PROTECT, related_name="keyword", verbose_name="주제")
     keyword = models.ForeignKey(Keyword, on_delete=models.PROTECT, related_name="subject", verbose_name="키워드")
 
-class Edge(models.Model):
+class Edge(TimeStampMixin):
     owner = models.CharField(max_length=16, verbose_name="사용자")
     # 문자열 순서 상 작은 쪽인 keyword1, 큰 쪽이 keyword2에 들어간다.
     keyword1 = models.ForeignKey(Keyword, on_delete=models.PROTECT, related_name="edge_start", verbose_name="키워드1")
     keyword2 = models.ForeignKey(Keyword, on_delete=models.PROTECT, related_name="edge_end", verbose_name="키워드2")
 
-class Essay(models.Model):
+class Essay(TimeStampMixin):
     owner = models.CharField(max_length=16, verbose_name="사용자")
     subject = models.ForeignKey(Subject, on_delete=models.PROTECT, related_name="에세이", verbose_name="주제")
 
-class TopicSentence(models.Model):
+class TopicSentence(TimeStampMixin):
     essay = models.ForeignKey(Essay, on_delete=models.PROTECT, related_name="topic_sentence", verbose_name="에세이")
     content = models.CharField(max_length=300, blank=True, verbose_name="내용")
     order =  models.IntegerField(default=20, blank=True, verbose_name='순서')
 
-class TopciSentenceKeyword(models.Model):
+class TopciSentenceKeyword(TimeStampMixin):
     topic_senctence = models.ForeignKey(TopicSentence, on_delete=models.PROTECT, related_name="keyword", verbose_name="중심 문장")
     keyword = models.ForeignKey(Keyword, on_delete=models.PROTECT, related_name="topic_sentence", verbose_name="키워드")
 
-class Phase(models.Model):
+class Phase(TimeStampMixin):
     topic_sentence = models.ForeignKey(TopicSentence, on_delete=models.PROTECT, related_name="phase", verbose_name="중심 문장")
     content = models.TextField(blank=True)
     is_selected =  models.BooleanField(default=True, verbose_name="선택")
