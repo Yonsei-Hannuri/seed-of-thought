fae00861a704d2a0b6284a577395ba2eb241be2e
하지훈 <jnh03336@gmail.com>
2024-08-16 08:52:04 +0900

    feat: 시리얼라이저 수정

--- backend/cowriter/serializer.py
+++ backend/cowriter/serializer.py
@@ -1,50 +1,47 @@
 from rest_framework import serializers
 from cowriter.models import *
 
-class SubjectKeywordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubjectKeyword
-        fields = ['keyword']
-
 class SubjectSerializer(serializers.ModelSerializer):
-    # custom 필드를 read only로 하려면 아래 처럼 선언시에 read_only 값을 설정해줘야함
-    # https://stackoverflow.com/questions/75980386/django-declaring-a-read-only-field-on-serializer
-    keywords = SubjectKeywordSerializer(many=True, read_only=True)
     class Meta:
         model = Subject
-        fields = ['source', 'purpose','content', 'keywords']
-        # 시리얼라이즈를 통해서 Read작업과 Create작업에 필요한 인수를 구분하기
-        # https://stackoverflow.com/questions/31675803/should-i-write-two-different-serializers-for-requests-post-and-get
-        read_only_fields = [ 'keywords' ]
-
+        fields = ['source', 'purpose','content']
 
 class KeywordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Keyword
         fields = '__all__'
 
-class EdgeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Edge
-        fields = '__all__'
+    def is_valid(self, raise_exception=False):
+        # 기존의 유효성 검사를 먼저 수행합니다.
+        valid = super().is_valid(raise_exception=raise_exception)
+
+        if not valid:
+            return False
+        
+        keyword_name = self.validated_data.get('name')
+        
+        if keyword_name == '':
+            self.add_error('name', '키워드는 빈 문자열일 수 없습니다.')
+            valid = False
+
+        return True
 
 class EssaySerializer(serializers.ModelSerializer):
     class Meta:
         model = Essay
         fields = '__all__'
 
-class TopciSentenceKeywordSerializer(serializers.ModelSerializer):
+class EssayMindmapSerializer(serializers.ModelSerializer):
     class Meta:
-        model = TopciSentenceKeyword
-        fields = ['keyword']
-
-class TopicSentenceSerializer(serializers.ModelSerializer):
-    keywords = TopciSentenceKeywordSerializer(many=True)
-    class Meta:
-        model = TopicSentence
-        fields = ['essay', 'content','order', 'keywords']
+        model = EssayMindmap
+        fields = '__all__'
 
 class PhaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Phase
+        fields = '__all__'
+
+class PhaseHistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PhaseHist
         fields = '__all__'
