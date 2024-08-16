3cc8d975842f7e298e7db53de9f0de30b9dfcc23
하지훈 <jnh03336@gmail.com>
2024-08-16 19:26:48 +0900

    feat: 모델 수정

--- backend/cowriter/models.py
+++ backend/cowriter/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from common.models.BaseModelMixin import BaseModelMixin
 
 class Subject(BaseModelMixin):
-    subject_id = models.CharField(max_length=32, verbose_name="출처", primary_key=True, db_column="SUBJECT_ID")
+    subject_id = models.CharField(max_length=100, verbose_name="주제ID", primary_key=True, db_column="SUBJECT_ID")
     subject_purpose = models.CharField(max_length=50, verbose_name="목적", db_column="SUBJECT_PURPOSE")
     subject_content = models.TextField(blank=True, verbose_name="내용", db_column="SUBJECT_CONTENT")
+    del_yn = models.BooleanField(default=False, verbose_name="삭제여부", db_column="DEL_YN")
 
     class Meta:
         db_table = "SUBJECT"
@@ -20,8 +21,9 @@ class Keyword(BaseModelMixin):
 class Essay(BaseModelMixin):
     essay_id = models.AutoField(primary_key=True, verbose_name="에세이ID", db_column="ESSAY_ID")
     subject_id = models.ForeignKey(Subject, on_delete=models.PROTECT, related_name="essay", verbose_name="주제", db_column="SUBJECT_ID")
-    owner = models.CharField(max_length=32, verbose_name="사용자", db_column="OWNER_ID")
+    owner = models.CharField(max_length=100, verbose_name="사용자", db_column="OWNER_ID")
     complete_yn = models.BooleanField(default=False, verbose_name="완료여부", db_column="COMPLETE_YN")
+    del_yn = models.BooleanField(default=False, verbose_name="삭제여부", db_column="DEL_YN")
 
     class Meta:
         db_table = "ESSAY"
@@ -35,19 +37,21 @@ class EssayMindmap(BaseModelMixin):
     class Meta:
         db_table = "ESSAY_MINDMAP"
 
-class Phase(BaseModelMixin):
-    phase_id = models.AutoField(primary_key=True, verbose_name="단락ID", db_column="PHASE_ID")
-    essay_id = models.ForeignKey(Essay, on_delete=models.PROTECT, related_name="phase", verbose_name="에세이", db_column="ESSAY_ID")
-    phase_content = models.TextField(blank=True, verbose_name="내용", db_column="PHASE_CONTENT")
+class Paragraph(BaseModelMixin):
+    paragraph_id = models.AutoField(primary_key=True, verbose_name="단락ID", db_column="PARAGRAPH_ID")
+    essay_id = models.ForeignKey(Essay, on_delete=models.PROTECT, related_name="paragraph", verbose_name="에세이", db_column="ESSAY_ID")
+    paragraph_content = models.TextField(blank=True, verbose_name="내용", db_column="PARAGRAPH_CONTENT")
+    order = models.IntegerField(default=0, verbose_name="순서", db_column="ORDER")
     del_yn = models.BooleanField(default=False, verbose_name="삭제여부", db_column="DEL_YN")
 
     class Meta:
-        db_table = "PHASE"
+        db_table = "PARAGRAPH"
 
-class PhaseHist(BaseModelMixin):
-    phase_id = models.ForeignKey(Phase, on_delete=models.PROTECT, related_name="history", verbose_name="단락", db_column="PHASE_ID")
+class ParagraphHist(BaseModelMixin):
+    paragraph_id = models.ForeignKey(Paragraph, on_delete=models.PROTECT, related_name="history", verbose_name="단락", db_column="PARAGRAPH_ID")
     change_type = models.CharField(max_length=8, verbose_name="변경타입", db_column="CHANGE_TYPE")
-    phase_content = models.TextField(blank=True, verbose_name="내용", db_column="PHASE_CONTENT")
+    change_command = models.TextField(blank=True, verbose_name="변경명령", db_column="CHANGE_COMMAND")
+    paragraph_content = models.TextField(blank=True, verbose_name="내용", db_column="PARAGRAPH_CONTENT")
 
     class Meta:
-        db_table = "PHASE_HIST"
+        db_table = "PARAGRAPH_HIST"
