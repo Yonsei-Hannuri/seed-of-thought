5e730b922e9747a56fac7d8b77ed61e1eae5763b
하지훈 <jnh03336@gmail.com>
2024-10-12 16:52:18 +0900

    feat: transaction 적용

--- backend/hannuri/admin.py
+++ backend/hannuri/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db import transaction
 from hannuri.models import *
 from lib import utils
 import json
@@ -11,27 +12,29 @@ class SeasonAdmin(admin.ModelAdmin):
     fields = ('is_current','year', 'semester', 'title', 'leader', 'sessioner', 'socializer')
     
     def save_model(self, request, obj, form, change):
-        if obj.is_current==True:
+        with transaction.atomic():
 
-            pre_current_season = Season.objects.all().filter(is_current=True)
-            if pre_current_season and len(pre_current_season) == 1 and pre_current_season[0].pk != obj.pk:
-                season = pre_current_season[0]
-                season_sessions = season.session.all()
-                season_detgoris = Detgori.objects.all().filter(parentSession__in=season_sessions)
+            if obj.is_current==True:
 
-                season_total_count = defaultdict(int)
-                for detgori in season_detgoris:
-                    detgori_words = json.loads(detgori.words)
-                    for key, value in detgori_words.items():
-                        season_total_count[key] += int(value)
+                pre_current_season = Season.objects.all().filter(is_current=True)
+                if pre_current_season and len(pre_current_season) == 1 and pre_current_season[0].pk != obj.pk:
+                    season = pre_current_season[0]
+                    season_sessions = season.session.all()
+                    season_detgoris = Detgori.objects.all().filter(parentSession__in=season_sessions)
 
-                season_total_count_filtered = \
-                    utils.filter_dict(season_total_count, lambda x: x[1] > 30)
-                season.words = json.dumps(season_total_count_filtered)
-                season.is_current = False
-                season.save()
-    
-        super().save_model(request, obj, form, change)   
+                    season_total_count = defaultdict(int)
+                    for detgori in season_detgoris:
+                        detgori_words = json.loads(detgori.words)
+                        for key, value in detgori_words.items():
+                            season_total_count[key] += int(value)
+
+                    season_total_count_filtered = \
+                        utils.filter_dict(season_total_count, lambda x: x[1] > 30)
+                    season.words = json.dumps(season_total_count_filtered)
+                    season.is_current = False
+                    season.save()
+        
+            super().save_model(request, obj, form, change)   
 
 
 class ReadfileInline(admin.TabularInline):
@@ -48,16 +51,17 @@ class SessionAdmin(admin.ModelAdmin):
     fields = ('is_current', 'week', 'title')
 
     def save_model(self, request, obj, form, change):
-        if obj.is_current==True:
-            pre_current_session = Session.objects.all().filter(is_current=True)
-            for session in pre_current_session:
-                session.is_current = False
-                session.save()
-    
-        if not obj.season:
-            obj.season = Season.objects.get(is_current=True)
+        with transaction.atomic():
+            if obj.is_current==True:
+                pre_current_session = Session.objects.all().filter(is_current=True)
+                for session in pre_current_session:
+                    session.is_current = False
+                    session.save()
+        
+            if not change:
+                obj.season = Season.objects.get(is_current=True)
 
-        super().save_model(request, obj, form, change)   
+            super().save_model(request, obj, form, change)   
 
     def save_formset(self, request, form, formset, change):
         instances = formset.save(commit=False)
