e0c1eee68e959bdb0cb49981258815f7b253db56
하지훈 <jnh03336@gmail.com>
2025-01-04 14:10:47 +0900

    fix: 성능이슈, N+1 문제 해결

--- backend/hannuri/job/WordCountAggregateJob.py
+++ backend/hannuri/job/WordCountAggregateJob.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 class WordCountAggregateJob:
     def aggregate_word_count(self, detgori_ids):
-        sentence_words = SentenceWord.objects.filter(sentence__detgori_id__in=detgori_ids).all()
+        sentence_words = SentenceWord.objects.filter(sentence__detgori_id__in=detgori_ids).select_related('word').all()
         count = defaultdict(int)
         for sentence_word in sentence_words:
             count[sentence_word.word.word] += sentence_word.count
