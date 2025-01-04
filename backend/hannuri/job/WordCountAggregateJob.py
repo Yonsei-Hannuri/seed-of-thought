f9a9f8336506deb3d0e3f64c84810e0388b70862
하지훈 <jnh03336@gmail.com>
2025-01-04 12:22:17 +0900

    feat: 댓거리에 사용된 단어 합계 기능

---
+++ backend/hannuri/job/WordCountAggregateJob.py
@@ -0,0 +1,11 @@
+from hannuri.models import Word, SentenceWord
+from collections import defaultdict
+
+class WordCountAggregateJob:
+    def aggregate_word_count(self, detgori_ids):
+        sentence_words = SentenceWord.objects.filter(sentence__detgori_id__in=detgori_ids).all()
+        count = defaultdict(int)
+        for sentence_word in sentence_words:
+            count[sentence_word.word.word] += sentence_word.count
+        return count
+    
