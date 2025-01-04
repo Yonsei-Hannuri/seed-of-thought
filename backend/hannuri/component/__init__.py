dde71c82273ad15617551e8599d746951c6fc002
하지훈 <jnh03336@gmail.com>
2025-01-04 11:30:21 +0900

    remove: 문장 관련 elasticsearch 의존 제거

--- backend/hannuri/component/__init__.py
+++ backend/hannuri/component/__init__.py
@@ -2,10 +2,8 @@ from .DetgoriPdfTextExtracter.DetgoriPdfTextExtracter import DetgoriPdfTextExtra
 from .TextAnalyzer.TextAnalyzer import TextAnalyzer
 from .GoogleOauth.GoogleOauth import GoogleOauth
 from .ObjectStorage.S3 import S3
-from .DetgoriSentenceApi.DetgoriSentenceApi import DetgoriSentenceApi
 
 detgoriPdfTextExtracter = DetgoriPdfTextExtracter()
 textAnalyzer = TextAnalyzer()
 googleOauth = GoogleOauth()
 objectStorage = S3()
-detgoriSentenceApi = DetgoriSentenceApi()
--- backend/hannuri/view/__init__.py
+++ backend/hannuri/view/__init__.py
@@ -1,3 +1,2 @@
 from .custom import *
 from .model import *
-from .derived import *
